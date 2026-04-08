2c058e0293e21e1ebfb8831c77ae3ee9a7bfab56
Franklin Hatulan <fmhatulan2@gmail.com>
2026-04-07 22:15:48 -0700

    Initial project structure w/ Gradle

---
+++ src/main/java/com/zetaga/osu_pp_tracker/OsuPpTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.zetaga.osu_pp_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsuPpTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsuPpTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetaga/osu_pp_tracker/OsuPpTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetaga.osu_pp_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsuPpTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
